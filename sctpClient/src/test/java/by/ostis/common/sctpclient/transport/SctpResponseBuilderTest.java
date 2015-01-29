eb85b70cbdb8c26fc3b03dde0463461e4ffd97bd
Vitali Park <vitalipark@gmail.com>
2015-01-29 15:01:30 +0300

    unfinished test

--- sctpClient/src/test/java/by/ostis/common/sctpclient/transport/SctpResponseBuilderTest.java
+++ sctpClient/src/test/java/by/ostis/common/sctpclient/transport/SctpResponseBuilderTest.java
@@ -1,7 +1,12 @@
 package by.ostis.common.sctpclient.transport;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,8 +20,19 @@ import by.ostis.common.sctpclient.model.response.SctpResultType;
 import by.ostis.common.sctpclient.utils.SctpResponseHeaderFactory;
 import by.ostis.common.sctpclient.utils.constants.SctpCommandType;
 
+@RunWith(Parameterized.class)
 public class SctpResponseBuilderTest {
 
+    private byte[] bytes;
+
+    private int sdfas;
+    
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+        	{new byte[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 24, }
+        });
+}
+    
     @Test
     public void checkCommandResponseHeaderSuccessTest() {
 	try {
@@ -30,7 +46,6 @@ public class SctpResponseBuilderTest {
 	    SctpResponseHeader excpectedResponseHeader = SctpResponseHeaderFactory.createNewResponse(
 		    SctpCommandType.CHECK_ELEMENT_CMD, 0, SctpResultType.SCTP_RESULT_OK, 0);
 
-	    Assert.assertEquals(checkedResponseHeader.toString(), excpectedResponseHeader.toString());
 	} catch (TransportException e) {
 	    Assert.fail(e.getMessage());
 	}
@@ -49,34 +64,28 @@ public class SctpResponseBuilderTest {
 
 	    SctpResponseHeader excpectedResponseHeader = SctpResponseHeaderFactory.createNewResponse(
 		    SctpCommandType.CHECK_ELEMENT_CMD, 0, SctpResultType.SCTP_RESULT_OK, 0);
-	    
-	    Assert.assertNotSame(checkedResponseHeader.toString(), excpectedResponseHeader.toString());
-	    	
+
+	    Assert.assertThat(checkedResponseHeader.toString(), not(equalTo(excpectedResponseHeader.toString())));
+
 	} catch (TransportException e) {
 	    Assert.fail(e.getMessage());
 	}
     }
 
-    // private boolean checkResponseHeader(byte[] checedByteReponse,
-    // SctpCommandType commandType, int commandId,
-    // SctpResultType resultType, int argumentSize) throws TransportException{
-    //
-    // byte[] checkedBytesResponse = new byte[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    // };
-    // InputStream checkedInputStream = new ByteArrayInputStream(
-    // checkedBytesResponse);
-    //
-    // SctpResponseBuilder responseBuilder = new BytesSctpResponseBuilder();
-    // SctpResponse checkedResponse = responseBuilder
-    // .build(checkedInputStream);
-    // SctpResponseHeader checkedResponseHeader = checkedResponse.getHeader();
-    //
-    // SctpResponseHeader excpectedResponseHeader = SctpResponseHeaderFactory
-    // .createNewResponse(SctpCommandType.CHECK_ELEMENT_CMD, 0,
-    // SctpResultType.SCTP_RESULT_OK, 0);
-    //
-    // return
-    // checkedResponseHeader.toString().equals(excpectedResponseHeader.toString());
-    // }
+    private boolean checkResponseHeader(byte[] checedByteReponse, SctpCommandType commandType, int commandId,
+	    SctpResultType resultType, int argumentSize) throws TransportException {
+
+	byte[] checkedBytesResponse = new byte[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	InputStream checkedInputStream = new ByteArrayInputStream(checkedBytesResponse);
+
+	SctpResponseBuilder responseBuilder = new BytesSctpResponseBuilder();
+	SctpResponse checkedResponse = responseBuilder.build(checkedInputStream);
+	SctpResponseHeader checkedResponseHeader = checkedResponse.getHeader();
+
+	SctpResponseHeader excpectedResponseHeader = SctpResponseHeaderFactory.createNewResponse(
+		SctpCommandType.CHECK_ELEMENT_CMD, 0, SctpResultType.SCTP_RESULT_OK, 0);
+
+	return checkedResponseHeader.toString().equals(excpectedResponseHeader.toString());
+    }
 
 }
